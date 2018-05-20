b2e604dab21c0520a9ebbca41223d54fc6b20f9e
Next <Next@192.168.2.25>
2018-05-20 19:36:40 +0530

    first commit

---
+++ SPBootNgApp-01/src/main/java/org/app/SpBootNgApp01Application.java
@@ -0,0 +1,12 @@
+package org.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBootNgApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBootNgApp01Application.class, args);
+	}
+}
